ef2be21c1cfdfb8b0230e0e5c4b58ffd56ee78b3
Lucas Borsatto Simão <lucasborsattosimao@hotmail.com>
2023-07-03 18:36:49 -0300

    Adicionando logs para performance

--- src/expediente/web/expediente.controller.ts
+++ src/expediente/web/expediente.controller.ts
@@ -16,7 +16,13 @@ export class ExpedienteController {
   @Post('batidas')
   async createBatida(@Body() createBatidaDto: CreateBatidaRequestDto): Promise<CreateBatidaResponseDto> {
 
+    this.logger.log(`ExpedienteController::createBatida - Início de processamento`);
+
+    const startTime = new Date();
     const result = await this.expedienteService.create(createBatidaDto);
+
+    const endTime = new Date()
+    this.logger.log(`ExpedienteController::createBatida - processo durou ${endTime.getMilliseconds() - startTime.getMilliseconds()} milisegundos`);
 
     return new CreateBatidaResponseDto(result.dia, result.pontos);
   }
